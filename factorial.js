4e51b1019bf181d41e61d242f8523feaec3a37c7
kathecar <katherinnecardenas16@gmail.com>
2025-02-10 16:40:29 -0500

    ya esta con html solo falta css

--- factorial.js
+++ factorial.js
@@ -1,15 +1,19 @@
-const prompt = require("prompt-sync")();  //Se importa el paquete promp-sync
-
-let numero = Number(prompt("Escriba un numero")) //Se pide un numero al usuario
-
-function factorial(numero) {
+function factorial() {
      //Se crea una funcion que recibe un numero
-    
+
+     let numero = Number(prompt("Escriba un numero")) //Se pide un numero al usuario
+
+     if(isNaN(numero) || numero < 0) {
+        alert("Ingrese un numero valido y que no sea negativo, por favor 😁");
+        return;
+        }
+
      let resultado = 1; 
 
      for (let i = 1; i <= numero; i++) {
          resultado = resultado * i; 
      }
-     return resultado;
+
+    console.log(`El factorial de ${numero} es: ${resultado}`);
+    document.getElementById("resultado").textContent = `El factorial de ${numero} es: ${resultado}`;
 }
-console.log(factorial(numero));
